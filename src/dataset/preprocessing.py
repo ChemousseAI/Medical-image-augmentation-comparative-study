77f100faa4e67456bce0ea69179ac1668e387116
Chemousse Berdjouh <alaholadanqatar@gmail.com>
2026-06-17 16:41:11 +0100

    Delete src/dataset directory

--- src/dataset/preprocessing.py
+++
@@ -1,9 +0,0 @@
-
-import torchvision.transforms as transforms
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
-
-def normalize_images(images):
-    return [transform(img) for img in images]
